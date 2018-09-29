67f0a4e1930827c1386fc5bcbcb2fea5e647bbd3
Yubin Jamora <yubinjamora@Yubins-MacBook-Pro.local>
2018-09-29 10:22:16 -0400

    final

---
+++ fakeSpot.js
@@ -0,0 +1,33 @@
+// set grade array "A~F"
+var myGrades = [
+  "A", "B", "C", "D", "F"
+  ];
+
+// grade the review randomly
+var randomGrade = myGrades[Math.floor(Math.random()* myGrades.length)];
+
+// grab id where to put the grade 
+var fakeSpotRandomGrade = $('#tellAFriendBox_feature_div');
+
+// create new div for the grade
+var newFakeSpotDiv = $("<div id='random-grade-box'>" + "<h4> Reviews Grade </h4>" + randomGrade + "</div>")
+
+// add the new div 
+fakeSpotRandomGrade.append(newFakeSpotDiv);
+	
+
+$(document).ready(function () {  
+  var top = $('#random-grade-box').offset().top;
+  $(window).scroll(function (event) {
+    var boxScrollTop = $(this).scrollTop();
+    
+    if (boxScrollTop >= top) {
+      $('#random-grade-box').addClass('fixed');
+    }
+    
+    else {
+      $('#random-grade-box').removeClass('fixed');
+    }
+    
+  });
+});
